6cecabebc38e1ec62c6cd97300b88db0fac68362
meowhuan <3250315682@qq.com>
2025-02-10 21:56:10 +0800

    .

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,24 +3,12 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    "portfolio",
     {
-      text: "案例",
+      text: "文档",
       icon: "laptop-code",
       prefix: "demo/",
       link: "demo/",
       children: "structure",
-    },
-    {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
     },
   ],
 });
